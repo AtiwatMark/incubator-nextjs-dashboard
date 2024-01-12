16e912c06531c4ecef6ed393176f041d7bd5f2c4
Atiwat Punboonchu <atiwat.p@arise.tech>
2024-01-12 13:35:52 +0700

    DashBoardCommit 1

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "../ui/dashboard/sidenav";
+
+
+export default function Layout({ children}: { children: React.ReactNode}) {
+    return (
+        <div className="md:flex h-screen mx-auto">
+            <div>
+                <SideNav />
+            </div>
+            {children}
+        </div>
+    )
+}
